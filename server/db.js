194aa61c2ff2f3e0788ddec8daa6085a5534813d
noob210 <111210721+noob210@users.noreply.github.com>
2026-02-03 15:47:51 +0800

    Fix neon

--- server/db.js
+++ server/db.js
@@ -1,11 +1,9 @@
 import { Pool } from "pg";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+
 dotenv.config();
 
 export const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("neon.tech") ? { rejectUnauthorized: false } : false,
 });
